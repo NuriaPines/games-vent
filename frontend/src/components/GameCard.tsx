dde3d473676070de85b6eeb82317b3dd14b26612
Nuria Pinés <nuriapines@gmail.com>
2026-03-10 22:19:58 +0100

    Cambio el src de GameCard para mostrar las imagenes

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -10,7 +10,7 @@ export default function GameCard({ titulo, precio, imagen }: GameCardProps) {
   return (
     <div className="bg-[#0f0524] rounded-lg overflow-hidden flex flex-col shadow-2xl transition-transform hover:scale-105">
       <img 
-        src={`http://localhost:8000/${imagen}`} 
+        src={`http://127.0.0.1:8000/${imagen}`} 
         className="w-full aspect-[3/4] object-cover" 
         alt={titulo}
       />
